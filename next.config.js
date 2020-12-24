d8eaab1f9dfba466bff5314028ec54ff5ce9f0bb
adambarghouthi <adambargh@gmail.com>
2020-12-23 19:29:36 -0500

    add next-sass next-less next-css

--- next.config.js
+++ next.config.js
@@ -1,15 +1,34 @@
+const withSass = require('@zeit/next-sass');
+const withLess = require('@zeit/next-less');
+const withCSS = require('@zeit/next-css');
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
-})
+});
 
-module.exports = (phase, { defaultConfig }) => {
-  const config = {
-    ...defaultConfig,
-    i18n: {
-      locales: ['en', 'ar'],
-      defaultLocale: 'en',
+const isProd = process.env.NODE_ENV === 'production';
+
+// fix: prevents error when .less files are required by node
+if (typeof require !== 'undefined') {
+  require.extensions['.less'] = (file) => {};
+}
+
+module.exports = withBundleAnalyzer({
+  i18n: {
+    locales: ['en', 'ar'],
+    defaultLocale: 'en',
+  },
+  ...withCSS({
+    cssModules: true,
+    cssLoaderOptions: {
+      importLoaders: 1,
+      localIdentName: "[local]___[hash:base64:5]",
     },
-  };
-
-  return withBundleAnalyzer(config);
-};
+    ...withLess(
+      withSass({
+        lessLoaderOptions: {
+          javascriptEnabled: true,
+        },
+      })
+    ),
+  }),
+});
