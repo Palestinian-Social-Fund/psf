f8de843da76c7db44dd47e032ba39c7bb55f4d62
adambarghouthi <adambargh@gmail.com>
2021-01-02 19:48:32 -0500

    add config provider

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,9 +1,25 @@
+import { useRouter } from 'next/dist/client/router';
+import { ConfigProvider } from 'antd';
+import arEG from 'antd/lib/locale/ar_EG';
+import enUS from 'antd/lib/locale/en_US';
+
 import { wrapper } from 'src/store';
+import useLocalization from 'src/lib/localization/useLocalization';
 
 import 'styles/antd-custom.less';
 
 const App = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  const { dir } = useLocalization();
+  const { locale } = useRouter();
+
+  return (
+    <ConfigProvider
+      lang={locale === 'ar' ? arEG : enUS}
+      direction={dir}
+    >
+      <Component {...pageProps} />
+    </ConfigProvider>
+  );
 };
 
 export default wrapper.withRedux(App);
